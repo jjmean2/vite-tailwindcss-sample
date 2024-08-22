0282c3948f0452386448c50a5053952cea01536f
Jongwan Lee <jjmean2@gmail.com>
2024-08-23 07:02:40 +0900

    Tailwind VSCode

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,9 +1,13 @@
 const config = {
-  "singleQuote": true,
-  "semi": false,
-  "trailingComma": "all",
-  "printWidth": 100,
-  "arrowParens": "avoid"
+  singleQuote: true,
+  semi: false,
+  trailingComma: 'all',
+  printWidth: 100,
+  arrowParens: 'avoid',
+  plugins: [
+    require.resolve('prettier-plugin-organize-imports'),
+    require.resolve('prettier-plugin-tailwindcss'),
+  ],
 }
 
 module.exports = config
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useState } from 'react'
+// import './App.css'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <div className="text-red-500 flex-1">나 빨강</div>
+      <div className="">
+        <div className="text-xs">ABC</div>
+        <button className="hover:!btn-primary">버튼</button>
+        <button className="!btn-secondary">버튼</button>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -19,18 +21,14 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
